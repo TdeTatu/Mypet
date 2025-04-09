5fd906dda3df311dfd1ba1e94268f726116b9284
TdeTatu <jonatasluizgs@gmail.com>
2025-04-08 23:30:36 -0300

    forms pt2

--- crud1/MyPet/forms.py
+++ crud1/MyPet/forms.py
@@ -1,10 +1,34 @@
 from django import forms
 
 class CadastroUsarioForm(forms.Form):
-    cpf = forms.CharField()
-    nome = forms.CharField()
+    cpf = forms.CharField(label = 'Cpf', max_length=100)
+    nome = forms.CharField(label = 'Nome', max_length=100)
+    data_de_nasc = forms.DateField(label = 'Data de nascimento')
+    genero = forms.ChoiceField(label = 'Gênero')
+    endereco = forms.CharField(label = 'Endereço', max_length=100)
+    tipo_residencia = forms.ChoiceField(label = 'Tipo de residência')
+    email = forms.EmailField(label = 'E-mail', max_length=100)
+    telefone = forms.CharField(label = 'Telefone', max_length=100)
+    senha = forms.CharField(label = 'Senha', max_length=100)
+
+class CadastroAnimalForm(forms.Form):
+    especie = forms.ChoiceField(label = 'Espécie')
+    raca = forms.ChoiceField(label = 'Raça')
+    porte = forms.ChoiceField(label = 'Porte do animal')
+    sexo = forms.ChoiceField(label = 'Sexo')
+    dt_nascimento = forms.DateField(label = 'Data de nascimento' )
 
 
+class CadastroMonitorForm(forms.Form):
+    cpf = forms.CharField(label = 'Cpf', max_length=100)
+    nome = forms.CharField(label = 'Nome', max_length=100)
+    data_de_nasc = forms.DateField(label = 'Data de nascimento')
+    genero = forms.ChoiceField(label = 'Gênero')
+    endereco = forms.CharField(label = 'Endereço', max_length=100)
+    tipo_residencia = forms.ChoiceField(label = 'Tipo de residência')
+    email = forms.EmailField(label = 'E-mail', max_length=100)
+    telefone = forms.CharField(label = 'Telefone', max_length=100)
+    senha = forms.CharField(label = 'Senha', max_length=100)
 
 
 
--- crud1/MyPet/views.py
+++ crud1/MyPet/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import CadastroUsarioForm, CadastroAnimalForm, CadastroMonitorForm
 
 #from .models import Pessoa
 
@@ -7,10 +10,40 @@ def index(request):
     return render(request, "index.html")
 
 def cadastro(request):
-    return render(request, "MyPet/cadastro.html")
+
+    form = CadastroUsarioForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            cpf = form.cleaned_data['cpf']
+            nome = form.cleaned_data['nome']
+            cpf = form.cleaned_data['']
+            cpf = form.cleaned_data['']
+            cpf = form.cleaned_data['']
+            cpf = form.cleaned_data['']
+            cpf = form.cleaned_data['']
+
+
+
+
+
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "MyPet/cadastro.html", context)
 
 def cadastro_pet(request):
-    return render(request, "MyPet/cadastro_pet.html") 
+
+    form = CadastroAnimalForm()
+
+    context = {
+        'form': form
+    }
+
+
+    return render(request, "MyPet/cadastro_pet.html", context) 
 
 def telaprincipal(request):
     return render(request, "MyPet/telaprincipal.html")
